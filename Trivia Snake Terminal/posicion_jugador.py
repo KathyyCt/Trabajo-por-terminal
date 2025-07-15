0bfa21deb7472d92df8f7ef73ad32665c217674c
KathyyCt <kathkathyy1@gmail.com>
2025-07-15 13:16:10 -0300

    Update posicion_jugador.py

--- Trivia Snake Terminal/posicion_jugador.py
+++ Trivia Snake Terminal/posicion_jugador.py
@@ -2,10 +2,28 @@
 
 def posicion_jugador (tablero:list, es_correcta:bool, posicion:int) -> int:
 
-    mov = 1 if es_correcta else -1
+    """
+    Actualiza la posicion del jugador en el tablero segun si la respuesta es correcta
+    o incorrecta. tambien interpreta casillas especiales que modifican el movimiento.
+
+    Parametros:
+    tablero (list): lista de enteros que representa el tableto. Cada casilla puede tener:
+        - 0: sin efecto
+        - un numero > 0: casilla especial que avanza segun el numero.
+        - un numero < 0: casillas especial que retrocede segunn el numero.
+    es_correcta (bool): indica si es correcta (True) o incorrecta (False)
+    posicion (int): numero que representa la posicion actual del jugador en el tablero.
+
+    Retorna:
+    int: Nueva posicion del jugador despues del movimiento.
+    """
+    if es_correcta:
+        mov = 1
+    else:
+        mov = -1
+
     posicion += mov
-    if tablero[posicion] == 1:
-        posicion += mov
-    elif tablero[posicion] == 2:
-        posicion += mov * 2
+    if tablero[posicion] != 0:
+        posicion += mov * tablero[posicion]
+        
     return posicion
