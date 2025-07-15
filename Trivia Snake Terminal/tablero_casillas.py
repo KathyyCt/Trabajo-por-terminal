3c05e32e4e582d0fb044e34c0e9e58297a41af96
KathyyCt <kathkathyy1@gmail.com>
2025-07-15 13:17:28 -0300

    Update and rename tablero.py to tablero_casillas.py

---
+++ Trivia Snake Terminal/tablero_casillas.py
@@ -0,0 +1,8 @@
+from preguntas import *
+
+tablero = [0, 1, 0, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 1, 0, 
+           1, 1, 0, 0, 1, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0]
+
+indice =[]
+for i in range (len(preguntas)):
+    indice.append(i)
