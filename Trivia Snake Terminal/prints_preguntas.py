1e376689efd00d3a4382c9533ef4499f3d1e6c9a
KathyyCt <kathkathyy1@gmail.com>
2025-07-15 13:16:43 -0300

    Update prints_preguntas.py

--- Trivia Snake Terminal/prints_preguntas.py
+++ Trivia Snake Terminal/prints_preguntas.py
@@ -1,10 +1,16 @@
 
-def preguntas_prints (lista_posicion):
+def preguntas_prints (lista_posicion:dict):
 
     """
-    La funcion printea las preguntas con sus posibles respuestas
-    Por parametros obtiene una lista que ya esta posicionda
-    No hay retorno
+    Muestra por pantalla una pregunta junto con sus posibles respuestas.
+
+    Parametros:
+    lista_posicion (dict): diccionario que contiene la pregunta y sus tres opciones
+    de respuesta.
+    Debe tener las claves: "pregunta", "respuesta_a", "respuesta_b", "respuesta_c".
+
+    Retorno:
+    Ninguno
     """
 
     print("\n" + lista_posicion["pregunta"])
